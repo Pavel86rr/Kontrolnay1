6ad5f78389ac17627cd9f99f02e148feb47f16ca
«Pavel <kskles@yandex.ru>
2023-07-17 20:27:18 +0700

    Controlnay1.1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/Задача. Написать программу, которая из имеющегося массива строк формирует 
+//новый массив из строк, длина которых меньше, либо равна 3 символам.
+
+Console.WriteLine("Введите массив строк через пробел");
+string? stroka = Console.ReadLine();
+
+void Main()
+{
+    string[] Massiv = stroka.Split(' ');
+    var result = new string[Massiv.Length];
+    var realSize = 0;
+
+    foreach (var value in Massiv)
+    {
+        if (value.Length <= 3)
+        {
+            result[realSize] = value;
+            realSize++;
+        }
+    }
+
+    Console.Write("[");
+    Console.Write(string.Join("; ", result, 0, realSize));
+    Console.Write("]");
+}
+
+Main();
+
+
+
